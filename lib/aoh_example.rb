7fa4b8517d92d4d6c4c1e2dacde0df69dd637caf
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-11 20:39:51 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,7 +14,8 @@ def assembled_aoh
      {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
      {:name=>"Pat Monteseian", :occupation=>"Staff"},
      {:name=>"Kate Gompert", :occupation=>"None"},
-     {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
+     {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
+   ]
 end
 
 def literal_aoh
